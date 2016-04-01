44ac24ce5fcc49f2ee120ed35fa938146ec154ff
Jakub Draganek <jakub.draganek@gmail.com>
2016-04-01 23:12:04 +0200

    list construction not needed

--- 30minutestest.py
+++ 30minutestest.py
@@ -40,7 +40,7 @@ def interpolate(points):
     """ Interpolate given t,y range into 1s spaced sequence. """
     time, hr = zip(*points)
     spl = UnivariateSpline(time, hr)
-    time_stamps = list(range(int(time[-1])))
+    time_stamps = range(int(time[-1]))
     new_hrs = spl(time_stamps)
     return list(zip(time_stamps, new_hrs))
 
