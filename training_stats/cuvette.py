57644f271274a192b9a706833a851375e18cf8c9
RunningBrute <piotrekbas8@gmail.com>
2016-04-07 14:53:00 +0200

    cleanup in cuvette.py

--- training_stats/cuvette.py
+++ training_stats/cuvette.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import half_hour_test
+from half_hour_test import get_trkpts, decode_iso_time, get_time, convert_time, interpolate
+import sys
 from geopy.distance import great_circle
 
 def get_time_intervals(gpx_file):
@@ -14,46 +15,20 @@ def get_elevation(trkpt, namespace):
     if elevation_children is not None:
         return elevation_children.text
 
+
 def get_points(trkpts): 
     return [(trkpt.attrib.get('lat'), trkpt.attrib.get('lon')) for trkpt in trkpts]
 
 
-def get_total_distans_in_km(gpx_file):
-    ns, trkpts = get_trkpts(gpx_file)
-    points = get_points(trkpts)
-    sum_of_dist = 0
-    dists = []
-    for i in range(len(points)-1):
-        first_point = (points[i][0], points[i][1])
-        second_point = (points[i+1][0], points[i+1][1])
-        dist = great_circle(first_point, second_point).miles
-        sum_of_dist += dist
-        #print("Added dist in m: ", sum_of_dist * 1.609);
-        dists.append(sum_of_dist * 1.609)
-    dists.append(sum_of_dist * 1.609)
-    #print("Distans in m: ", sum_of_dist * 1.609)
-    #print("Dist len: ", len(dists))
-    #print("Time len: ", len(trkpts))
-    return dists
+def get_distans(gpx_file):
+    dists = get_distans_intervals(gpx_file)
+    return [sum(dists[0:i]) for i in range(1, len(dists))]
 
 
 def get_distans_intervals(gpx_file):
     ns, trkpts = get_trkpts(gpx_file)
     points = get_points(trkpts)
-    sum_of_dist = 0
-    dists = []
-    for i in range(len(points)-1):
-        first_point = (points[i][0], points[i][1])
-        second_point = (points[i+1][0], points[i+1][1])
-        dist = great_circle(first_point, second_point).miles
-        #sum_of_dist += dist
-        #print("Added dist in m: ", sum_of_dist * 1.609);
-        dists.append(dist * 1.609)
-    dists.append(dist * 1.609)
-    #print("Distans in m: ", sum_of_dist * 1.609)
-    #print("Dist len: ", len(dists))
-    #print("Time len: ", len(trkpts))
-    return dists
+    return [great_circle((points[i-1][0], points[i-1][1]), (points[i][0], points[i][1])).miles * 1.609 for i in range(1, len(points))]
 
 
 def get_elevation_measurements(gpx_file):
@@ -64,40 +39,8 @@ def get_elevation_measurements(gpx_file):
 
 
 def main():
-    time_intervals = get_time_intervals(gpx_file)
-    dists = get_total_distans_in_km(gpx_file)
-    dists_list = get_distans_intervals(gpx_file)
-    t2, elev2 = zip(*get_elevation_measurements(gpx_file))
+    gpx_file = sys.argv[1] 
+    print("Loading gpx: {}".format(gpx_file)) 
 
-    plot_elevation = False
-
-    # elevation in time
-    elevations = interpolate(get_elevation_measurements(gpx_file))
-    measured_elevation_time, measured_elevations = zip(*elevations[start_measure:stop_measure])
-
-
-    if(plot_elevation):
-        t, elev = zip(*elevations)
-        pyplot.subplot(412)
-        pyplot.plot(t, elev, 'g')
-        pyplot.plot(measured_elevation_time, measured_elevations, 'r')
-        pyplot.xlabel('Time [s]')
-        pyplot.ylabel('Elevation')
-
-        pyplot.subplot(413)
-        pyplot.plot(dists, elev2, 'g')
-        pyplot.xlabel('Distans [m]')
-        pyplot.ylabel('Elevation')
-
-        pace_intervals = [(time_intervals[i]/60)/(dists_list[i]) for i in range(len(time_intervals) - 1)]
-        time_sum = [(time_intervals[i] + time_intervals[i-1]) for i in range(1, len(time_intervals))]
-        time_sum = [sum(time_intervals[0:i]) for i in range(1, len(time_intervals))]
-        
-        #print pace_intervals
-        #print time_intervals
-        #print time_sum
-
-        pyplot.subplot(414)
-        pyplot.plot(time_sum, pace_intervals, 'b')
-        pyplot.xlabel('Time [s]')
-        pyplot.ylabel('Pace [min/km]')
+if __name__ == "__main__":
+    main()
