6af08b66266e23969e4697980b9920c2940c8822
Jakub Draganek <jakub.draganek@gmail.com>
2016-04-09 20:39:03 +0200

    handling gpx file upload stub

--- webgui/views.py
+++ webgui/views.py
@@ -1,8 +1,21 @@
 from flask import render_template
+from werkzeug import secure_filename
 from webgui import web_gui
 
 @web_gui.route('/')
 def index():
+    ''' Render landing page '''
     return render_template('index.html',
                            title='Home')
+
+@web_gui.route('/upload', methods=['POST'])
+def upload():
+    ''' Serve upload file request '''  
+    gpx = request.files['file']
+    if gpx and is_gpx(gpx.filename):
+        filename = secure_filename(gpx.filename)
+        gpx.save(os.path.join(web_gui.config['USER_GPX_FOLDER'], filename))
+        return render_template('training.html',
+                               filename=filename)
+        
 
