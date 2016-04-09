c6777b29a7341fde7b1495185d40d43c2f8ef4ea
Jakub Draganek <jakub.draganek@gmail.com>
2016-04-09 20:25:48 +0200

    index.html template

--- webgui/views.py
+++ webgui/views.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from webgui import web_gui
 
 @web_gui.route('/')
 def index():
-    return 'Hello world!'
+    return render_template('index.html',
+                           title='Home')
 
