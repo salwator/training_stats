947d7dff050aa20216daec981aa29f44ddf2bf1f
Jakub Draganek <jakub.draganek@gmail.com>
2016-04-09 20:19:45 +0200

    flask in dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ config = {
     'author': 'Jakub Draganek',
     'author_email': 'jakub.draganek@gmail.com',
     'version': '0.1',
-    'install_requires': ['nose', 'numpy', 'matplotlib', 'geopy'],
+    'install_requires': ['flask', 'nose', 'numpy', 'matplotlib', 'geopy'],
     'packages': ['training_stats'],
     'scripts': [],
     'name': 'training_stats'
