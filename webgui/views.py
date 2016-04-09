0c7c62c769b813548003d0ee4ea33641064cc6d6
Jakub Draganek <jakub.draganek@gmail.com>
2016-04-09 21:04:34 +0200

    upload is working

--- webgui/views.py
+++ webgui/views.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, request
 from werkzeug import secure_filename
 from webgui import web_gui
+import os
 
 
 @web_gui.route('/')
@@ -14,6 +15,10 @@ def index():
 def upload():
     ''' Serve upload file request '''
     gpx = request.files['file']
+    
+    def is_gpx(filename):
+        return filename.rsplit('.', 1)[1].lower() == 'gpx'
+    
     if gpx and is_gpx(gpx.filename):
         filename = secure_filename(gpx.filename)
         gpx.save(os.path.join(web_gui.config['USER_GPX_FOLDER'], filename))
--- webgui/webgui.py
+++ webgui/webgui.py
@@ -1,4 +1,4 @@
 from flask import Flask
 
 web_gui = Flask(__name__)
-web_gui.config['USER_GPX_FOLDER'] = 'uploaded_gpx/'
+web_gui.config['USER_GPX_FOLDER'] = 'webgui/uploaded_gpx/'
