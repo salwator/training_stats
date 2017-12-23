4d9685760dfda3fd7475503e7ae67e73c68d4318
Jakub Draganek <jakub.draganek@gmail.com>
2017-12-23 17:38:34 +0100

    Move tests

---
+++ test/test_get_hr_measurements.py
@@ -0,0 +1,6 @@
+from training_stats.gpxfile import get_hr_measurements
+
+
+def test_get_hr_measurements():
+    hr = get_hr_measurements("./gpx/test_hr_short.gpx")
+    assert hr == [(0, 73), (1, 73), (11, 82), (12, 90), (13, 96)]
