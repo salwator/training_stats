ddff26751e3414c9262859870c52c4267fef9d27
Jakub Draganek <jakub.draganek@gmail.com>
2017-12-23 18:10:23 +0100

    Update package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 config = {
     'name': 'training_stats',
     'packages': ['training_stats'],
-    'version': '0.1.0',
+    'version': '0.2.0',
     'description': 'Training Stats',
     'author': 'Jakub Draganek',
     'author_email': 'jakub.draganek@gmail.com',
